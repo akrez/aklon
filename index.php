9203690fdb6b2e6d26a981bb5eaa1095b0601b32
Akrez <akrez.like@gmail.com>
2024-01-16 01:01:02 +0330

    schema

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require('./Aklon.php');
 
 $baseHost = 'localhost/filter/aklon2';
 
-$fakeUrl = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$fakeUrl = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
 $realUrl = Aklon::decryptUrl($fakeUrl, $baseHost);
 
